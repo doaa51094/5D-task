41660d00aefddda0d043fc8f3134a1cba359745f
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-07-02 18:59:19 +0300

    create About me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import Responsability from "../../assets/Responsability (2).png";
-import Colombian from'../../assets/Group 28.png';
+import Colombian from "../../assets/Group 28.png";
+import Highlight from "../../assets/Highlight_04.png";
+import Patron6 from "../../assets/Patron de Puntos 6.png";
+import Patron5 from "../../assets/Patron de Puntos5.png";
+
+
 const About = () => {
   return (
-    <div className="about flex justify-center items-center">
-        <div className="vectorCuadros">      
-  <div className="relative flex justify-center items-center  w-3/4">
-    <img src={Colombian} alt="Colombian" className="w-full"/>
-  </div>
+    <div className="about relative flex flex-col justify-center items-center">
+      <h2 className="text-white font-semibold lg:text-[56px] text-[40px] leading-[67.2px] pt-12">
+        About Me
+      </h2>
+      <div className="vectorCuadros flex justify-center items-center">
+        <div className="relative flex justify-center items-center  w-[75%] mb-40">
+          <img src={Colombian} alt="Colombian" className="w-full" />
+          <div className="absolute lg:top-28 top-0  lg:-right-6 -right-6">
+            <img src={Highlight} alt="Highlight" className="md:w-32 w-16" />
+          </div>
         </div>
-</div>
-
+      </div>
+      <div className="absolute top-[30%] left-0">
+        {" "}
+        <img src={Patron6} alt="Patron6" className="lg:w-16 w-12" />
+      </div>
+      <div className="absolute bottom-[15%] right-0">
+        {" "}
+        <img src={Patron5} alt="Patron5" className="lg:w-[120px] w-16" />
+      </div>
+    </div>
   );
 };
 
